9ff4f75fcff17ae9909487cf65e52f4cec14bfd0
Psalm Diao <p.diao25@gmail.com>
2023-12-30 00:28:03 +0100

    Compute weighted point total #1

--- app.js
+++ app.js
@@ -12,24 +12,59 @@ function collect_grades() {
   };
 }
 
+function select_accredited_courses() {
+  return {
+    lk1: [],
+    lk2: [],
+    ab3: [],
+    ab4: [],
+    gk1: [],
+    gk2: [],
+    gk3: [],
+    gk4: [],
+    gk5: [],
+  };
+}
+
+function map_subject_labels() {
+  return {};
+}
+
 function calculate(e) {
   e.preventDefault();
 
   let grades = collect_grades();
   // todo: check if there are "0" grades
 
-  // calculate intensive course (LK) grades
-  let points = 2 * grades.lk1.concat(grades.lk2).reduce((a, b) => {
-    return a + b;
-  }, 0);
-  points = grades.ab3.concat(grades.ab4).reduce((a, b) => {
-    return a + b;
-  }, 0);
-  let nr_of_courses = 24; // 16 LK und mind. 20 und max. 26 GK
-  
+  let accredited_courses = select_accredited_courses();
+
+  // calculate point total
+  let points = 0;
+  let nr_of_courses = 0; // 8 (16) LK und mind. 20 und max. 26 GK
+  for (let subject in accredited_courses) {
+    accredited_courses[subject].forEach((e, i) => {
+      if (e && grades.hasOwnProperty(subject)) {
+        let weighted_points = grades[subject][i];
+        points += subject.includes('lk') ? weighted_points * 2 : weighted_points;
+        nr_of_courses++;
+      }
+    });
+  }
+
+  while (nr_of_courses < 34) {
+    break;
+  }
+  console.log(points, nr_of_courses);
+
   // deutsch, mathematik, fremdsprache (falls noch keine LKs)
   // aufgabenfeld 2 (gesellschaftlich) mind. 2 aufeinanderfolgende semester oder religion
   // aufgabenfeld 3 (naturwissenschaftlich) mind. 2 aufeinanderfolgende semester
+  // +4, +4, +4
+
+
+  //nr_of_courses += 8; // intensive course grades are multiplied by two
+
+  let result_one = points / nr_of_courses;
 }
 
 document.querySelector('#form-button').addEventListener('click', calculate);
